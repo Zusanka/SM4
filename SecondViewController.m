74bc0095173efb840a8464615eb5e0e20d528cc5
Zusanka <131263973+Zusanka@users.noreply.github.com>
2023-11-07 10:03:02 +0100

    Add files via upload

---
+++ SecondViewController.m
@@ -0,0 +1,20 @@
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.modifiedSurnameTextField.text = self.surname;
+}
+
+-(void) toGoBack {
+    NSString *itemToPassBack = self.modifiedSurnameTextField.text;
+    [self.delegate addItemViewController:self didFinishEnteringItem:itemToPassBack];
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
---
+++ ViewController.h
@@ -0,0 +1,12 @@
+
+#import <UIKit/UIKit.h>
+#import "SecondViewController.h"
+@interface ViewController : UIViewController <SecondViewControllerDelegate>
+
+@property (nonatomic, weak) IBOutlet UILabel *messageLabel;
+@property (nonatomic, weak) IBOutlet UITextField *inputField;
+@property (nonatomic, weak) IBOutlet UITextField *surnameTextField;
+
+-(IBAction)enter;
+@end
+
